1f20a013c7748c6f8703dfc4ffa40e70a43e7d1f
Jackson Ming Hu <huming2207@gmail.com>
2022-09-19 22:51:10 +1000

    lib: add cmdbroker

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use panic_probe as _;
 use stm32wlxx_hal as _;
 
 pub mod mono;
+pub mod cmd_broker;
 
 // same panicking *behavior* as `panic-probe` but doesn't print a panic message
 // this prevents the panic message being printed *twice* when `defmt::panic` is invoked
